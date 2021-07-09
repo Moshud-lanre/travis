49f94d702619ffed4f21a25125c17d6b987b95ab
moshud-lanre <lanremoshud@gmail.com>
2021-07-10 00:15:07 +0100

    environment variable

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-
+const test = process.env.FAT;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    const test = process.env.FAT;
+   
     console.log("Microservices rock!");
     console.log(test);
     await sleep(5000);
